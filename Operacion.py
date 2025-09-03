7742543a84fcbd74d0530d8c7ba0da26872d6a88
equispeq2-zoid <equispeq2@fcpn.edu.bo>
2025-09-03 12:01:53 -0400

    Add files via upload

---
+++ Operacion.py
@@ -0,0 +1,7 @@
+# Operación simple
+numero1 = 5
+numero2 = 10
+suma = numero1 + numero2
+print("La suma de", numero1, "y", numero2, "es:", suma)
+print("1 Edgar Juan Quispe Quispe 8353100”)
+print("2 Iver Bladimir Ticona 8460597”)
---
+++ Ssludo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Saludo {
+ public static void main(String[] args) {
+ System.out.println("¡Hola, equipo!");
+ System.out.println("1 Edgar juan quispe 8353100");
+ System.out.println("2 Iver Bladimir Ticona 8460597");
+
+ }
+}
